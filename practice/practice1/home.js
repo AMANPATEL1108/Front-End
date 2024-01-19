af915f0a6632efb7f245d1f3f60c458bf7e45a44
AMAN PATEL <amanpatel8844382@gmail.com>
2024-01-19 08:34:01 +0530

    download and view add

--- practice/practice1/home.js
+++ practice/practice1/home.js
@@ -10,3 +10,17 @@ function showContent(contentId) {
 
     document.getElementById(contentId).style.display = 'block';
 }
+
+function viewImage(imageSrc) {
+    window.open(imageSrc);
+}
+
+function downloadImage(imageSrc) {
+    // Create an anchor element and trigger a download
+    var link = document.createElement('a');
+    link.href = imageSrc;
+    link.download = 'downloaded_image.jpg';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
